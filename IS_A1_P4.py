5a54f87767a64b8952f7ba4a89ada005010aa604
kal-nemi <vishal.mca20.du@gmail.com>
2021-10-21 18:12:53 +0530

    Add Program 4 and 5

---
+++ IS_A1_P4.py
@@ -0,0 +1,4 @@
+# 4. Write a program that can perform a letter frequency attack on any monoalphabetic 
+# substitution cipher without human intervention. Your software should produce possible
+# plain text in rough order of likelihood. It would be good if your user interface allows user to specify " 
+# Give me top 10 possible plain texts"
